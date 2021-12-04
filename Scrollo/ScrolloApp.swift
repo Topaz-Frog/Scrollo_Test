875340a36f68e2805311a0e1ddcae0e2c8b57950
TopazFrog <63302251+Sapphire-Ninja@users.noreply.github.com>
2021-12-04 19:39:39 +0100

    Initial Commit

---
+++ Scrollo/ScrolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrolloApp.swift
+//  Scrollo
+//
+//  Created by Erwin Marysiok on 04/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
